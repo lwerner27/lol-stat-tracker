4caad2c8b42ff70f24eea14258ae627aa3d0d1bb
Lucas Werner <lwerner27@gmail.com>
2020-07-09 15:23:02 -0500

    Did some work on the basic structure of the site.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// Page Imports
+import Home from "./pages/Home";
+
+// Custom Components
 import Navbar from "./components/Navbar";
 
 import "./scss/main.scss";
 
-function App() {
-    return (
-        <div className="App">
-            <Navbar />
-        </div>
-    );
+class App extends React.Component {
+    // Constructor Function
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiUlr: "",
+        };
+    }
+
+    render() {
+        return (
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
